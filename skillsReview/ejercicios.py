961c3a94eeb65e389da6035f38c289b12b173a01
paleto30 <paleto0817@gmail.com>
2023-02-15 15:50:42 -0500

    Ejercicio 1
    SkillsReview de introduccion

---
+++ skillsReview/ejercicios.py
@@ -0,0 +1,16 @@
+""" 
+que operadores Utiliza python en los siguientes casos 
+
+A) Division modular
+B) Exponenciacion
+c) Division que retorne entero 
+
+"""
+
+n1 = 36
+n2 = 3
+
+print('A) Division modular = %\nB) Exponenciacion = **\nc) Division que retorne entero = //\n')
+print(f'Esto es una Division modular:\n{n1} % {n2} = {n1%35}\n')
+print(f'esto es una Exponenciacion:\n{n1} ** {n2} = {n1**36}\n')
+print(f'esto es una Divison que retorna entero:\n{n1} // {n2} = {n1//36}\n')
