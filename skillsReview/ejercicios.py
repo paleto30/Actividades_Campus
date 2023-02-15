c8a0d81ca8716df9cbef1f405cd6fe5a2c70b4fe
paleto30 <paleto0817@gmail.com>
2023-02-15 16:03:25 -0500

    Ejercicio 4

--- skillsReview/ejercicios.py
+++ skillsReview/ejercicios.py
@@ -1,13 +1,29 @@
 """ 
-3. Si hay operadores de igual precedencia, en qué orden se
-ejecutan?
-
-A. De izquierda a derecha
-B. De derecha a izquierda 
+ejercicio 4: 
+    Que son las expresiones regulares en Python?
 """
 
-print('Suma y resta tienen igual prioridad ,por tanto se evaluan de izquierda a derecha\nejemplo:\n')
-print(f'operacion:  {2} + {2} - {4} = {2+2-4}')
-print(f'notamos que la respuesta es {2+2-4}.\n')
 
-print('\n si son de igual precedencia  se ejecutan de izquierda a derecha')
+print('Las expresiones regulares son patrones utilizados para encontrar \nuna determinada combinación de caracteres dentro de una cadena de texto. \nLas expresiones regulares proporcionan una manera muy \nflexible de buscar o reconocer cadenas de texto.')
+print('\ndentro de Python a través del módulo re. Usando este pequeño lenguaje, \nespecificas las reglas para el conjunto de cadenas de caracteres posibles que deseas hacer coincidir;\nejemplo:\n')
+
+import re
+
+regex = re.compile('a[3-5]+') 
+print(regex,' : # coincide con una letra, seguida de al menos 1 dígito entre 3 y 5')
+
+cadena = 'a4123'
+print('\ndato a verificar: ' ,cadena)
+print(regex.match(cadena))
+print(regex.search(cadena))
+
+cadena2 = 'a7345'
+print('\ndato a verificar: ',cadena2) 
+print(regex.match(cadena2))
+print(regex.search(cadena2))
+
+
+
+
+
+
