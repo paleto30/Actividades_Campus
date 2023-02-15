0fd7e8e4272875715baa9ec326b8192565e4f590
paleto30 <paleto0817@gmail.com>
2023-02-15 15:53:01 -0500

    Ejercicio 2
    Skillsreview de introduccion

--- skillsReview/ejercicios.py
+++ skillsReview/ejercicios.py
@@ -1,16 +1,56 @@
 """ 
-que operadores Utiliza python en los siguientes casos 
-
-A) Division modular
-B) Exponenciacion
-c) Division que retorne entero 
+2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa?
 
 """
+# primer grado
+print('\nEn la jerarquía de operadores, cuáles son \nlos que más prioridad tienen cuando el intérprete de Python los evalúa?\n')
+print('1) las operaciones entre  (  ) tienen la mayor prioridad')
+print(f'ejemplo:\n {12} * {4} + ({4} * {3}) = ',(12 * 4 + (4 * 3)),'\n')
 
-n1 = 36
-n2 = 3
+# segundo grado
+print('2) las potencias tienen el segundo grado de prioridad\nejemplo:')
+print(f'{6} ** {2} + {4}  = {6**2+4}\n' )
 
-print('A) Division modular = %\nB) Exponenciacion = **\nc) Division que retorne entero = //\n')
-print(f'Esto es una Division modular:\n{n1} % {n2} = {n1%35}\n')
-print(f'esto es una Exponenciacion:\n{n1} ** {n2} = {n1**36}\n')
-print(f'esto es una Divison que retorna entero:\n{n1} // {n2} = {n1//36}\n')
+# tercer grado
+print('3) Multiplicacion y division, modulo o residuo, division entera,\ntienen el tercer grado de prioridad\n')
+print(f'ejemplo\n{10} * {6} / {2} // {5} % {2} = {10*6/2//5%2}')
+
+print(f'{10} * {6} = {10*6}')
+print(f'{10*6} / {2} = {10*6/2}')
+print(f'{10*6/2} // {5} = {10*6/2//5}')
+print(f'{10*6/2//5} % {2} = {10*6/2//5%2}\n')
+
+
+# cuarto grado 
+print('4) la suma y la resta tienen el cuarto grado de prioridad\nejemplo')
+print(f'{2} * {5} + {4} = {2*5+4} , como vemos primero multiplica y luego hace la suma\nesto es por que le da prioridad a la multiplicacion y luego a la suma')
+print('\nel mismo ejemplo anterior pero esta vez con resta\n')
+print(f'{2} * {5} - {4} = {2*5-4} , como vemos primero multiplica y luego hace la suma\nesto es por que le da prioridad a la multiplicacion y luego a la sresta\n')
+
+
+# quinto grado 
+print('5) los Operadores relacionales tienen el 5 grado de prioridad\n-menor que: <\n-menor igual que: <=\n-mayor que: >\n-mayor igual que: >=')
+print('-diferente de: !=\n igual igual que: ==\n')
+print('ejemplo:\n')
+num1, num2 = 20, 30
+print(f'numero 1 = {num1},  numero 2 = {num2}\n{num1} < {num2} = {num1 < num2}')
+print(f'\n{num1} <= {num2} = {num1 <= num2}')
+print(f'\n{num1} > {num2} = {num1 > num2}')
+print(f'\n{num1} >= {num2} = {num1 >= num2}')
+print(f'\n{num1} != {num2} = {num1 != num2}')
+print(f'\n{num1} == {num2} = {num1 == num2}')
+
+
+#sexto grado
+
+print('\n\n6) operador logico AND tiene el sexto grado de prioridad\nejemplos:)')
+print(f'supongamos que tenemos dos numeros:\nnumero1 = {5}\nnumero2 = {8}\n')
+print(f'ahora se tiene que cumplir la siguiente condicion:\nsi numero1 Y numero2 son multiplos de 2\nentonces la respuesta debe ser True:\n')
+respuesta = None
+if 5 % 2 == 0 and 8 % 2 == 0 :
+    respuesta = True
+else:
+    respuesta = False
+print(f'si {5} % 2 == 0 and {8} % 2 == 0\nrespuesta: {respuesta}\n')
+print('como vemos la respuesta es falsa, ya que 5 no es multiplo de 2.\ntambien notamos que primero se realizan las operaciones y luego la verificacoon del operador AND')
