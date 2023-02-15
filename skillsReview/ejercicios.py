be7da26be290410f3f7878583a8f70711b47df48
paleto30 <paleto0817@gmail.com>
2023-02-15 16:07:47 -0500

    Ejercicio 6

--- skillsReview/ejercicios.py
+++ skillsReview/ejercicios.py
@@ -1,26 +1,19 @@
 """ 
-5. ejercicio
- 
-Enumere 5 tipos de datos en Python y suministre un valor de
-ejemplo de cada uno.
+Ejercicio 6. 
+
+En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos.
 """
 
-print('tipos de datos en python\n')
-n1 = 40
-n2 = 99291817389127398173987321918236219836129
-n3 = 3.1416
-n4 = True
-cadena = 'Hello word'
-lista = [1,2,3,4] 
-tupla = ('uno','dos','tres')
 
-print(f'dato entero = {n1} ',type(n1))
-print(f'dato entero largo = {n2}', type(n2))
-print(f'dato decimal = {n3}',type(n3))
-print(f'dato booleano = {n4}', type(n4))
-print(f'dato cadena o String = {cadena}', type(cadena))
-print(f'dato de tipo Array  = {lista}', type(lista))
-print(f'dato de tipo tupla  = {tupla}', type(tupla))
+print('¿En sus propias palabras, qué son las funciones\npreconstruidas y proporcione 2 ejemplos.?\n')
+print('rta:  estas funciones son las que  ya vienen incluidas en python \ny tenemos muchas funciones preconstruidas')
+print('por ejemplo: funcion max , funcion min\nnos sirven para encontrar el maximo valor y el minimo valor.')
+lista = [1,5,4,2,6,7,32,3]
+print(f'\ntenemos la siguiente lista de datos: {lista}')
+print('y yo quiero saber cual es mayor numero que hay en la lista rapidamente.\nentonces uso la funcion preconstruida de python max() de la siguiente manera')
+print(f'max(lista) esto retorna = {max(lista)}\n')
+print(f'min(lista) esto retorna = {min(lista)}\n')
 
 
 
