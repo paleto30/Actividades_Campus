5628f1a8587671cf7a730a2fec5f93edab82c495
paleto30 <paleto8017@gmail.com>
2023-03-08 06:36:15 -0500

    feat ( replace ) : usabilidad del metodo replace

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
+/*  
+    metodo replace()
+*/
 
-/* Metodos de los  Strings */
-
-/* es  posible crear cadenas usando comillas simples o dobles */
-let cadena1 = "comillas dobles "
-let cadena2 = 'comillas simples' 
-let cadena3 = `backtick`;
-console.log(`${cadena1}\n${cadena2}\n${cadena3}`);
-
+let cadena = "Esto es una cadena con comillas dobles y solo remplaza la primera sub-cadena que encuentra"
+console.log(cadena.replace('cadena','String'));
 
 
 /* 
-    metodo includes() , startsWith(), endsWith();
+    este metodo nos permite remplazar una subcadena por la palabra que queramos
+    pero si dentro de la cadena hay dos o mas palabras igual , solo remplaza ç
+    la primera que encuentra , inicia la busqueda de izquierda a derecha
 */
 
-//includes()  con este metodo podemos buscar una  sub especifica cadena dentro de una cadena 
-const cadena = "Esto es una cadena  de comillas dobles"
-console.log(cadena.includes('les'));  // este metodo puede recibir dos parametros , 
-                                      // el primero es la sub cadena, el segundo si se reuiere , 
-                                      // la poscicion de dicha subcadena
-console.log(cadena.includes('z',8));
-
-
-
-/* 
-    startsWith() con ese metodo podemos verificar si una cadena comienza con una determinada subcadena
-*/
-
-const frase = 'Hola soy Andres';
-console.log(frase.startsWith('h')); // esto retorna falso por el case sensitive
-
-
-/* 
-    endsWith()  
-*/
-
-const frase1 = 'Hola soy Andres';
-console.log(frase.endsWith('des')); // esto debe retornar verdadero por que termina en s
