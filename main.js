46abc925e92a3a952fda5951020926d84ce14f55
paleto30 <paleto0817@gmail.com>
2023-03-07 19:50:57 -0500

    feat ( var ) : se realizo el ejemplo de variable de tipo var

---
+++ main.js
@@ -0,0 +1,12 @@
+/* 
+    definicion de una variable  con var (o variable de tipo global)
+*/
+if (true) {
+    var variable = 'Andres Galvis'; 
+}
+console.log(`Definicion de variabble con var.\nMY VAR ES: ${variable}`);
+
+/* 
+se puede observar que el ambito de la variable no se limita solo al bloque
+de coigo donde ah sido declarada  
+*/
