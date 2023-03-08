9221776f32889df228523ef48174321758c87f72
paleto30 <paleto0817@gmail.com>
2023-03-07 21:28:06 -0500

    feat ( Strings ) :  Metodos tipo String  filtrado de  sub cadenas

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
+
+/* Metodos de los  Strings */
+
+/* es  posible crear cadenas usando comillas simples o dobles */
+let cadena1 = "comillas dobles "
+let cadena2 = 'comillas simples' 
+let cadena3 = `backtick`;
+console.log(`${cadena1}\n${cadena2}\n${cadena3}`);
+
+
+
 /* 
-    datos de tipo primitivos
+    metodo includes() , startsWith(), endsWith();
 */
 
-// 1)  booleano :  true y flase 
-const flag1 =  true;
-const flag2 = false;
-console.log(`Esto es verdad: ${flag1}\nEsto es falso: ${flag2}`);
+//includes()  con este metodo podemos buscar una  sub especifica cadena dentro de una cadena 
+const cadena = "Esto es una cadena  de comillas dobles"
+console.log(cadena.includes('les'));  // este metodo puede recibir dos parametros , 
+                                      // el primero es la sub cadena, el segundo si se reuiere , 
+                                      // la poscicion de dicha subcadena
+console.log(cadena.includes('z',8));
 
 
-// 2) null, para del lenguaje que denota un valor nulo.
-const nulo = null;
-console.log(`esto es un dato nulo: ${nulo}`);
+
+/* 
+    startsWith() con ese metodo podemos verificar si una cadena comienza con una determinada subcadena
+*/
+
+const frase = 'Hola soy Andres';
+console.log(frase.startsWith('h')); // esto retorna falso por el case sensitive
 
 
-// 3) tipo de dato indefinido  (el valor no esta definido)
-const indefinido = undefined;
-console.log(`tipo de dato indefinido: ${indefinido}`);
+/* 
+    endsWith()  
+*/
 
-
-// 4) tipo de dato numerico (el valor puede ser de tipo entero(12) o  flotante(12.2123123))
-const numero = Number(12.564);
-console.log(`tipo de dato numero:  ${typeof(numero)}    Number: ${numero}`);
-
-
-// 5) BigInt.  Un numero entero con precision arbitraria. ej: 95999123125491293199123n
-
-const number =  BigInt( 2n ** 53n);
-console.log(`tipo de dato BigInt: ${number.constructor.name}    BigInt: ${number} `);
-
-// 6) String. Una secuencia de caracteres que representan un valor de texto
-const cadena =  String('Esto es una cadena');
-console.log(`tip de dato String: ${cadena.constructor.name}     String: ${cadena}`);
-
-
-// 7) Symbol : Nuevo tipo de dato , de tipo primitivo , sirve para crear identificadores que son unicos
-let id = Symbol();
-let id2 = Symbol();
-console.log(`verificacion de la igualdad dato Symbol: ${id === id2}`);
-console.log(id);
+const frase1 = 'Hola soy Andres';
+console.log(frase.endsWith('des')); // esto debe retornar verdadero por que termina en s
