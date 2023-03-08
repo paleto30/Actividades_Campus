39230e6e521560dcc4fe397ee158c3ea9ac66027
paleto30 <paleto8017@gmail.com>
2023-03-08 07:35:59 -0500

    feat ( substring ) : usabilidad del metodo substring

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /* 
-    metodo slice()
+    metodo substring()
 */
 
-let cadena = "Esto es una cadena y yo voy a sacar una sub cadena de este texto";
+let cadena = "Esto es una cadena y yo voy a sacar una sub cadena de este texto usando el metodo substring";
 
-console.log(cadena.slice(30, 64));
+console.log(cadena.substring(30, 91));
 
 /*  
     como vemos el metodo nos extrae la cadena dentro del rango de el caracter
-    30 hasta el 64:
+    30 hasta el 91:
 
-    sub-cadena= sacar una sub cadena de este texto
+    sub-cadena= sacar una sub cadena de este texto usando el metodo substring
 */
 
 
