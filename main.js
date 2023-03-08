3ffd42fa1b783d5f81a5271601496d604e40fe25
paleto30 <paleto8017@gmail.com>
2023-03-08 09:01:39 -0500

    feat ( codePointAt ) : usabilidad de codePointAt

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 /* 
-    metodo .charCodeAt()
+    metodo .codePointAt()
 */
 
 
 let cadena = "Andrea";
 let cadena1 = cadena.toLowerCase();
-console.log(`cadena:${cadena} codigo-char: ${cadena.charCodeAt(0)}`);
-console.log(`cadena:${cadena1} codigo-char: ${cadena1.charCodeAt(0)}`);
+console.log(`cadena:${cadena} codePointAt: ${cadena.codePointAt(0)}`);
+console.log(`cadena:${cadena1} codePointAt: ${cadena1.codePointAt(0)}`);
 
 /*  
-    El charCodeAt() método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.
-    Un entero entre 0 y 1 menos que la longitud de la cadena; si no se especifica, su valor predeterminado es 0.
+   El método codePointAt() devuelve un entero no negativo que equivale al valor Unicode code point del carácter.
+   Índice del carácter en la cadena del que se quiere obtener el valor del Unicode code point.
 */
-
