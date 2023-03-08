f6424923ac28cec0e8a1d482bd9f26763def1fd9
paleto30 <paleto8017@gmail.com>
2023-03-08 08:15:02 -0500

    feat ( charAt ) : usabilidad de el metodo charAt

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /* 
-    metodo .length
+    metodo .charAt()
 */
 
 
 let cadena = "Esto es una cadena, con algunos caracteres para contar por medio del metodo length ";
-console.log(`la cadena tiene: ${cadena.length} caracteres`);
+
+console.log(cadena.charAt());
+console.log(cadena.charAt(12));
 
 /*  
-    la propiedad .length , nos devuelve la cantidad de caracteres que posee una cadena de texto 
+    El método charAt() de String devuelve en un nuevo String el carácter UTF-16 de una cadena.
+    Un entero entre 0 y 1 menos que la longitud de la cadena. Si no se proporciona ningún indice charAt() utilizará 0.
 */
 
