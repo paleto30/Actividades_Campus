83d6800c7fc3dabaf9faa2b1530466d64e77f64d
paleto30 <paleto0817@gmail.com>
2023-03-09 08:23:25 -0500

    feat (matchAll ): :rocket: usabilidad de el metodo matchAll

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 /* 
-    metodo .match()
+    metodo .matchAll()
 */
 
-const paragraph = 'Este texto Tiene Palabras en Mayuscula ';
-const regex = /[A-Z]/g;
-const found = paragraph.match(regex);
+const regexp = /t(e)(st(\d?))/g;
+const str = 'test1test2';
 
-console.log(found);
+const array = [...str.matchAll(regexp)];
+
+console.log(array[0]);
+console.log(array[1]);
 
 
 /*  
-   El método match() devuelve todas las 
-   ocurrencias de una expresión regular dentro de una cadena.
-   devuelve un array con el filtrado por medio de la expresion regular 
+    El método matchAll() retorna un iterador de todos los
+    resultados de ocurrencia en una cadena de texto contra 
+    una expresión regular, incluyendo grupos de captura.
+
+    cadena.matchAll(expresionRegular)
 */
