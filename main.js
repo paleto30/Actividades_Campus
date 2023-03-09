6ba922b2875c73957a3a8d12a921c3dab8eef554
paleto30 <paleto0817@gmail.com>
2023-03-09 08:08:24 -0500

    feat(match): :rocket: usabilidad del metodo match

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 /* 
-    metodo .localeCompare()
+    metodo .match()
 */
 
-const a = 'réversa'; // Con acentos, minúsculas
-const b = 'REVERSA'; // sin acentos, mayusculas
+const paragraph = 'Este texto Tiene Palabras en Mayuscula ';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
 
-console.log("Ejemplo 1:",a.localeCompare(b));
-
-console.log("ejemplo 2:",a.localeCompare(b, 'es', { sensitivity: 'base' }));
-
+console.log(found);
 
 
 /*  
-   El método localeCompare() retorna un número 
-   indicando si una cadena de carateres de 
-   referencia va antes, después o si es la 
-   misma que la cadena dada en orden alfabético. 
-
-   localeCompare(compareString, locales, options);
-
-   Los nuevos argumentos locales y options permiten a las 
-   aplicaciones especificar el idioma cuyo orden alfabético 
-   se debe usar y configurar el comportamiento de la función. 
-   En implementaciones antiguas, que ignoran los argumentos 
-   locales y options, la localización y el orden alfabético 
-   usados son completamente dependientes de la implementación. 
+   El método match() devuelve todas las 
+   ocurrencias de una expresión regular dentro de una cadena.
+   devuelve un array con el filtrado por medio de la expresion regular 
 */
