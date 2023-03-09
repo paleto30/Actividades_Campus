04780cf809232947b33771baa9a45438720f20ad
paleto30 <paleto0817@gmail.com>
2023-03-09 08:03:54 -0500

    feat(localeCompare): :rocket: usabilidad de el metodo localeCompare

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 /* 
-    metodo .lastIndexOf()
+    metodo .localeCompare()
 */
 
+const a = 'réversa'; // Con acentos, minúsculas
+const b = 'REVERSA'; // sin acentos, mayusculas
 
-let cadena = "Soy un camper de campus ";
-console.log(cadena.lastIndexOf('u'));
-console.log(cadena.lastIndexOf('u', 4));
+console.log("Ejemplo 1:",a.localeCompare(b));
+
+console.log("ejemplo 2:",a.localeCompare(b, 'es', { sensitivity: 'base' }));
+
+
 
 /*  
-   El método lastIndexOf() devuelve la posicion 
-   (indice) en la que se encuentra el valorBusqueda, 
-   dentro del objeto String que realiza la llamada, 
-   de la última ocurrencia del valor especificado; 
-   o -1 si no se halla. La búsqueda se realiza empezando 
-   por el final de la cadena que realiza la llamada, empezando 
-   en indiceDesde.
+   El método localeCompare() retorna un número 
+   indicando si una cadena de carateres de 
+   referencia va antes, después o si es la 
+   misma que la cadena dada en orden alfabético. 
+
+   localeCompare(compareString, locales, options);
+
+   Los nuevos argumentos locales y options permiten a las 
+   aplicaciones especificar el idioma cuyo orden alfabético 
+   se debe usar y configurar el comportamiento de la función. 
+   En implementaciones antiguas, que ignoran los argumentos 
+   locales y options, la localización y el orden alfabético 
+   usados son completamente dependientes de la implementación. 
 */
