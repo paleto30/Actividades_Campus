834417e42ce7336d2c1749688d2d4d54de34e55c
paleto30 <paleto0817@gmail.com>
2023-03-09 07:47:43 -0500

    feat( lastIndexOf ): :rocket:  usabilidad de el metodo lastIndexOf

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 /* 
-    metodo .indexOf()
+    metodo .lastIndexOf()
 */
 
 
 let cadena = "Soy un camper de campus ";
-console.log(cadena.indexOf('de',5));
+console.log(cadena.lastIndexOf('u'));
+console.log(cadena.lastIndexOf('u', 4));
 
 /*  
-   El método indexOf() devuelve el índice, dentro del objeto 
-   String que realiza la llamada, de la primera ocurrencia 
-   del valor especificado, comenzando la búsqueda desde 
-   indiceDesde; o -1 si no se encuentra dicho valor.
-
-   sintaxis: 
-        cadena.indexOf(valorBusqueda[, indiceDesde])
+   El método lastIndexOf() devuelve la posicion 
+   (indice) en la que se encuentra el valorBusqueda, 
+   dentro del objeto String que realiza la llamada, 
+   de la última ocurrencia del valor especificado; 
+   o -1 si no se halla. La búsqueda se realiza empezando 
+   por el final de la cadena que realiza la llamada, empezando 
+   en indiceDesde.
 */
