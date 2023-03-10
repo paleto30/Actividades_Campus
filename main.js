29a8688f1290b08a2c7afdefae7758c39679c4dd
paleto30 <paleto0817@gmail.com>
2023-03-10 07:54:31 -0500

    feat(toFixed): :rocket: usabilidad del metodo toFixed de Number

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 /* 
     propiedades y metodos NUMBER
 */
-//  Metodo .toExponential
+//  Metodo .toFixed
 
-function expo(x, f) {
-    return Number.parseFloat(x).toExponential(f);
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
   }
   
-  console.log(expo(123456, 2));
-
-  console.log(expo('123456'));
-
-  console.log(expo('oink'));
+  console.log(financial(123.456));
+  
+  console.log(financial(0.004));
+  
+  console.log(financial('1.23e+5'));
 
 /* 
-El método toExponential() devuelve una cadena que 
-representa el objeto Número en notación exponencial.
+El método toFixed() formatea un 
+número usando notación de punto fijo.
 */
