e928c972831ffe7b6d5343f0dc68d1f2298f7acd
paleto30 <paleto0817@gmail.com>
2023-03-10 15:37:54 -0500

    feat (math.random): :rocket: utilidad de el metodo math.random()

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 /* 
     metodos Math
 */
-//  Metodo .pow()  
+//  Metodo .random()  
 
-const datos = 12;
-console.log(Math.pow(datos,2));
+function getRamdomInt(max) {
+    return Math.floor(Math.random()* max)
+}
+
+console.log(getRamdomInt(3));
+
+console.log(getRamdomInt(1));
+
+console.log(Math.random());
 
 /* 
-el metodo pow me permite elevar una base a un exponente indicado 
+El método estático Math.random() devuelve un número 
+pseudoaleatorio de punto flotante que es mayor o igual 
+que 0 y menor que 1, con una distribución aproximadamente 
+uniforme en ese rango, que luego puede escalar a su rango deseado.
 */
