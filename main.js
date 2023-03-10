8e9b78421f9adb38d348b95f4498dafd627d6f6a
paleto30 <paleto0817@gmail.com>
2023-03-10 08:09:46 -0500

    feat(parseInt): :rocket: funcionalidad del metodo parseInt

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 /* 
     propiedades y metodos NUMBER
 */
-//  Metodo .isNaN()
+//  Metodo Number.parseInt()
 
-function typeOfNaN(x) {
-    if (Number.isNaN(x)) {
-      return 'Number NaN';
+function roughScale(x, base) {
+    const parsed = Number.parseInt(x, base);
+    if (Number.isNaN(parsed)) {
+      return 0;
     }
-    if (isNaN(x)) {
-      return 'NaN';
-    }
+    return parsed * 100;
   }
   
-  console.log(typeOfNaN('100F'));
-  console.log(typeOfNaN(NaN));
+  console.log(roughScale(' 0xF', 16));
+  
+  console.log(roughScale('321', 2));
 
+  let num = '15';
+  console.log(typeof(num));
+  let n1 = Number.parseInt(num);
+  console.log(typeof(n1));
 /* 
-
-
-El método estático Number.isNaN() determina 
-si el valor pasado es el valor numérico NaN y 
-devuelve falso si la entrada no es del tipo Número.
+El método estático Number.parseInt() 
+analiza un argumento de cadena y devuelve 
+un número entero de la raíz o base especificada.
 */
