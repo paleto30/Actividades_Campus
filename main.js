3d10d7d0dd3e7dd6a0b953c316b3ac097d1785a2
paleto30 <paleto0817@gmail.com>
2023-03-10 07:42:47 -0500

    feat(EPSILON): :rocket: propiedad epsilon de Number

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /* 
     propiedades y metodos NUMBER
 */
-// propiedad .MIN_VALUE
+// propiedad .EPSILON
 
-console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON);
 
 /* 
-La propiedad de datos estáticos Number.MIN_VALUE 
-representa el valor numérico positivo más pequeño representable en JavaScript.
+La propiedad de datos estáticos Number.EPSILON 
+representa la diferencia entre 1 y el número de 
+punto flotante más pequeño mayor que 1.
 */
