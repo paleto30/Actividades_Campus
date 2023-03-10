eea28f53f7143191afb8834d8861b2dfda9c9e01
paleto30 <paleto0817@gmail.com>
2023-03-10 08:12:27 -0500

    feat(parseFloat): :rocket: funcionalidad del metodo parseFloat

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 /* 
     propiedades y metodos NUMBER
 */
-//  Metodo Number.parseInt()
+//  Metodo Number.parseFloat()
 
-function roughScale(x, base) {
-    const parsed = Number.parseInt(x, base);
-    if (Number.isNaN(parsed)) {
+function circumference(r) {
+    if (Number.isNaN(Number.parseFloat(r))) {
       return 0;
     }
-    return parsed * 100;
+    return parseFloat(r) * 2.0 * Math.PI ;
   }
   
-  console.log(roughScale(' 0xF', 16));
-  
-  console.log(roughScale('321', 2));
+  console.log(circumference('4.567abcdefgh'));
+  console.log(circumference('abcdefgh'));
 
-  let num = '15';
-  console.log(typeof(num));
-  let n1 = Number.parseInt(num);
-  console.log(typeof(n1));
+  
 /* 
-El método estático Number.parseInt() 
-analiza un argumento de cadena y devuelve 
-un número entero de la raíz o base especificada.
+El método estático Number.parseFloat() 
+analiza un argumento y devuelve un número 
+de coma flotante. Si no se puede analizar 
+un número del argumento, devuelve NaN.
 */
