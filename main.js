734298de69ad8e1027b3b55f46b7b3afb8d8616f
paleto30 <paleto0817@gmail.com>
2023-03-11 11:22:12 -0500

    feat ( Math.sin ): :rocket: usabilidad del metodo sin de math

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 /* 
     metodos Math
 */
-//  Metodo .cos()   
+//  Metodo .sin()   
 
 
-function getCircleX(radians, radius) {
-    return Math.cos(radians) * radius;
+function getCircleY(radians, radius) {
+    return Math.sin(radians) * radius;
   }
   
-  console.log(getCircleX(1, 10));
-
-  console.log(getCircleX(2, 10));
-
-  console.log(getCircleX(Math.PI, 10));
+  console.log(getCircleY(1, 10));
   
+  console.log(getCircleY(2, 10));
 
+  console.log(getCircleY(Math.PI, 10));
+
+  
 /* 
-El método estático Math.cos() devuelve el 
-coseno de un número en radianes.
+El método estático Math.sin() devuelve 
+el seno de un número en radianes.
 */
