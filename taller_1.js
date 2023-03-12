3a4da2f688473169e19045f9e4b6ef1151e41361
paleto30 <paleto0817@gmail.com>
2023-03-11 20:48:34 -0500

    fea ( Ejercicio 2 ): :rocket: verificar si un numero es par o impar.

--- taller_1.js
+++ taller_1.js
@@ -1,22 +1,13 @@
 /* 
-    ejercicio # 1
+    ejercicio # 2
 
-    Construir el algoritmo para un programa que ingrese tres
-    notas de un alumno, si el promedio es menor o igual a 3.9
-    mostrar un mensaje "Estudie“, de lo contrario un mensaje que
-    diga "becado"
+    Dado un número indicar si es par o impar y si es mayor de 10.
 */
 
 do {
-    let notas = 0;
-    let count = 0;
-    for(let i = 1; i<=3 ; i++){
-        let nota = Number(prompt(`Ingrese la nota numero ${i}`))
-        notas += nota;
-        count ++;
-    }
-    console.log(count);
-    let result = notas/count
-    alert(`su nota es: ${result.toFixed(1)}\n${(result.toFixed(1) < 4 ? "Debes Estudiar Mas": "Te has ganado una Beca." )}`);
+  
+    let numero = Number(prompt("Ingrese un numero Entero: "))
+    let resultado = numero % 2 == 0 ? `el numero ${numero} es par`: `el numero ${numero} es impar.`
+    alert(resultado)
 
-} while (confirm("¿ Desea verificar otras notas ?"));
+} while (confirm("¿ Desea verificar otro numero ?"));
