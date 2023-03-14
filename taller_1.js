3aa18f9f4f34ea4ddb87bf27841b68d00bf99e31
paleto30 <paleto0817@gmail.com>
2023-03-13 22:35:45 -0500

    feat ( Ejercicio 6 ): :rocket: ejercicio 6 terminado.

--- taller_1.js
+++ taller_1.js
@@ -1,21 +1,49 @@
 /* 
-    ejercicio # 5
+    ejercicio # 6
     
-    Construir el algoritmo que lea por teclado dos números,
-    si el primero es mayor al segundo informar su suma y
-    diferencia, en caso contrario, informar el producto y la
-    división del primero respecto al segundo.
+    Construir el algoritmo en Javascript para un programa
+    para cualquier cantidad de estudiantes que lea el nombre,
+    el sexo y la nota definitiva y halle al estudiante con la mayor
+    nota y al estudiante con la menor nota y cuantos eran
+    hombres y cuantos mujeres.
 */
 
-do {
-    
-    let n1 = Number(prompt("Ingrese el numero 1: "))
-    let n2 = Number(prompt("Ingrese la numero 2: "))
-    
-    const result = n1 > n2 ? `la suma de los numeros es: ${n1+n2}
-                            \nla diferencia de los numeros es: ${n1-n2}`
-                            : `el producto de los numero es: ${n1*n2}
-                            \nla division de los numeros es: ${n1/n2}`
 
-    alert(result)
-} while (confirm("¿ Desea verificar otras personas ?"));
+do {
+    const array = [];
+    let flag = true;
+    while (flag) {
+        let nombre = String(prompt("Ingrese el Nombre: "))
+        let sexo = String(prompt("Ingrese el Genero: (M/F) "))
+        let nota = Number(prompt("Ingrese la Nota definitiva: (0.0/0.5) "))
+        sexo = sexo.toUpperCase();
+        const estudiantes = {
+            nombre,
+            sexo,
+            nota,
+        }
+        array.unshift(estudiantes)
+        flag = confirm("¿ Desea agregar otro estudiante ?")
+    }
+
+    let mayor = array.sort((e1,e2) => (e1.nota < e2.nota ) ? 1:(e1.nota > e2.nota) ? -1 : 0)
+    console.log("mayor: ",mayor[0]);
+    console.log("menor: ",mayor.slice(-1)[0]);
+    
+    let M = 0;
+    let F = 0;
+    array.forEach(element => {
+        if (element.sexo === "F") {
+            F += 1
+        }else{
+            M += 1
+        }
+    });
+
+    console.log(`Total Hombres: ${M}\nTotal Mujer: ${F}`);
+    console.log(array);
+
+    alert(`Mayor Nota:\nNombre: ${mayor[0].nombre}\nGenero: ${mayor[0].sexo}\nNota: ${mayor[0].nota}\n\nMenor Nota:\nNombre: ${mayor.slice(-1)[0].nombre}\nGenero: ${mayor.slice(-1)[0].sexo}\nNota: ${mayor.slice(-1)[0].nota}\n\nCantidad Total de hombres: ${M}\nCantidad Total de Mujeres: ${F}`)
+    
+    
+} while (confirm("¿ Desea Realizar un nuevo Registro ?"));
