3bfea66dc1a877ce58f7b6a285fb87c7ea666cf4
paleto30 <paleto0817@gmail.com>
2023-03-18 18:06:47 -0500

    feat ( Taller 3 ): :rocket: Ejercicio terminado , segun mis criterios :v

--- taller3/datosSede.js
+++ taller3/datosSede.js
@@ -17,16 +17,22 @@ formSede.addEventListener("submit", (e) => {
         contacto:data.contacto,
         team:[],
         campers:[],
-        trainers:[]
+        trainers:[],
+        roadmap:[],
+        asignaturas:[]
     }
     formSede.reset();
     //console.log(campus);
     const sedeTeam = document.querySelector("#tsedes");
     const trsedes = document.querySelector("#trSedes");
     const cpsedes = document.querySelector("#cpSedes");
+    const asedes = document.querySelector("#asedes");
+    const conSedes = document.querySelector("#conSedes");
+    listarSedes(asedes);
     listarSedes(sedeTeam);
     listarSedes(trsedes);
     listarSedes(cpsedes);
+    listarSedes(conSedes);
 })
 
 
@@ -54,13 +60,26 @@ formTeam.addEventListener("submit",(e)=>{
 }) 
 
 
+
+//formulario para asignaturas
+
+const formAsignatura = document.querySelector("#asignaturaf");
+formAsignatura.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    campus[data.sede]['asignaturas'].push(data); 
+    formAsignatura.reset();
+    console.log(campus);
+});
+
+
 // para cuando dan click en la sede de Trainers
 const sedeTrainerSelect = document.querySelector("#trSedes");
 sedeTrainerSelect.addEventListener("click",(e)=>{
     e.preventDefault();
-    listarTeams(sedeTrainerSelect.value)
+    listarTeams(sedeTrainerSelect.value);
+    listarAsignaturas(sedeTrainerSelect.value);
 })
-
 
 // funcion que lista el team segun el tipo de dato que se le pase 
 function listarTeams(sede) {
@@ -69,11 +88,24 @@ function listarTeams(sede) {
     for (let [k,v] of Object.entries(campus[sede]["team"])) {
         //console.log(v.nombre);
         etiquetaTrainers.insertAdjacentHTML("beforeend",`
-        <option value="${k}">${v.nombre} - ${v.jornada} - ${v.horario} </option> 
+        <option value="${v.nombre}">${v.nombre} | ${v.jornada} | horario ${v.horario} </option> 
         `)
     }
-     
 }
+
+// funcion que lista  las asignaturas segun la sede
+function listarAsignaturas(sede) {
+    const asignaturaTrainer = document.querySelector("#materiaT");
+    asignaturaTrainer.innerHTML = null;
+    for(let [k,v] of Object.entries(campus[sede]["asignaturas"])){
+        asignaturaTrainer.insertAdjacentHTML("beforeend",`
+        <option value="${[v.nombre,v.modalidad]}">${v.nombre} - ${v.modalidad}</option> 
+        `)
+    }
+}
+
+
+
 
 /* 
 |   Registrar un trainer 
@@ -87,38 +119,87 @@ formTrainers.addEventListener('submit',(e)=>{
     console.log(campus);
 });
 
-
-
-
-
-
 // teams para campers
 const selectSedeCamper = document.querySelector("#cpSedes");
-console.log(selectSedeCamper);
+//console.log(selectSedeCamper);
 selectSedeCamper.addEventListener('click',(e)=>{
     e.preventDefault();
     listarTeamsC(selectSedeCamper.value);
 })
 
+// funcion para listar los equipos disponibles  en el formulario 
 function listarTeamsC(sede) {
     const etiqueta = document.querySelector("#teamsC");
     etiqueta.innerHTML = null;
     for (let [k,v] of Object.entries(campus[sede]["team"])) {
         etiqueta.insertAdjacentHTML("beforeend",`
-            <option value="${k}">${v.nombre} - ${v.jornada} - ${v.horario} </option> 
+        <option value="${v.nombre}">${v.nombre} | ${v.jornada} | horario ${v.horario} </option> 
         `)
     }
 }
 
-
-/* 
-    Regitrar un camper 
-*/
+// Regitrar un camper 
 const formCampers = document.querySelector("#camper");
 formCampers.addEventListener('submit',(e)=>{
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target))
-    campus[`${data.sedeC}`]['campers'] = data;
+    campus[`${data.sedeC}`]['campers'].push(data);
     formCampers.reset();
+    console.log(data);
     console.log(campus);
+
+});
+
+
+//===========================================================================================================
+//consultas
+
+const consulta1 = document.querySelector("#consulta1");
+//console.log(consulta1);
+consulta1.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const sedeConsulta = document.querySelector("#conSedes").value;
+    consultaUno(sedeConsulta);    
 })
+
+function consultaUno(sede) {
+    console.log("       --TRAINERS--\n"); 
+    campus[sede]['trainers'].forEach( element => {
+        const { nombre, asignatura } = element; 
+        console.log(`Trainer: ${nombre} | Asignatura: ${asignatura}`);
+    });
+
+    console.log("       --CAMPERS--\n"); 
+    campus[sede]['campers'].forEach( element => {
+        const { nombre, salon } = element; 
+        console.log(`Camper: ${nombre} | Salon: ${salon}`);
+    });
+
+}
+
+const consulta2 = document.querySelector("#consulta2"); 
+consulta2.addEventListener("click",(e)=>{
+    e.preventDefault();
+    try {     
+        console.log("Telefono de Sede Medellin: ",campus.Medellin.contacto);
+        console.log("\nDireccion de Sede Bucaramanga: ",campus.Bucaramanga.direccion);
+    } catch (error) {
+        console.log("La sede no esta registrada");
+    }
+})
+
+
+const consulta3 = document.querySelector("#consulta3");
+consulta3.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const sedeConsulta = document.querySelector("#conSedes").value;
+    consultaTres(sedeConsulta);
+})
+
+
+function consultaTres(sede) {
+    
+    campus[sede]['asignaturas'].forEach(element => {
+        console.table(element);
+    })
+}
