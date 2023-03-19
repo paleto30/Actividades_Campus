d483474173a6dd5c9cd6f72244d5cd14bef2fa52
paleto30 <paleto0817@gmail.com>
2023-03-19 10:39:05 -0500

    feat ( Clausuras ): :rocket: que es una clausura y como es su funcionamiento

--- main.js
+++ main.js
@@ -5,30 +5,40 @@
 
 /* 
 
-  funciones Autoejecutables 
+  funciones Clausuras 
 
-  Pueden existir casos en los que necesites crear una 
-  función y ejecutarla sobre la marcha. En Javascript 
-  es muy sencillo crear funciones autoejecutables. Básicamente, 
-  sólo tenemos que envolver entre paréntesis la función anónima 
-  en cuestión (no necesitamos que tenga nombre, puesto que no la 
-  vamos a guardar) y luego, ejecutarla:
+  A grandes rasgos, en Javascript, una clausura o 
+  cierre se define como una función que «encierra» 
+  variables en su propio ámbito (y que continúan 
+  existiendo aún habiendo terminado la función). 
+  Por ejemplo, veamos el siguiente ejemplo:
   
 */
 
-// Función autoejecutable
-(function () {
-  console.log("Hola!!");
+// Clausura: Función incr()
+const incr = (function () {
+  let num = 0;
+  return function () {
+    num++;
+    return num;
+  };
 })();
+
+typeof incr; // 'function'
+console.log(incr()); // 1
+console.log(incr()); // 2
+console.log(incr()); // 3
 
 
 /* 
-De hecho, también podemos utilizar parámetros en 
-dichas funciones autoejecutables. Observa que sólo 
-hay que pasar dichos parámetros al final de la función autoejecutable. 
-*/
+Tenemos una función anónima que es también una 
+función autoejecutable. Aunque parece una función 
+por expresión, no lo es, ya que la variable incr 
+está guardando lo que devuelve la función anónima 
+autoejecutable, que a su vez, es otra función diferente.
 
-// Función autoejecutable con parámetros
-(function (name) {
-  console.log(`¡Hola, ${name}!`);
-})("Andres");
+La «magia» de las clausuras es que en el interior 
+de la función autoejecutable estamos creando una variable 
+num que se guardará en el ámbito de dicha función, por lo 
+tanto existirá con el valor declarado: 0. 
+*/
