b9a59c80f2a6ba4823053682865f9c843f367447
paleto30 <paleto0817@gmail.com>
2023-03-19 11:23:51 -0500

    feat ( Objetos FormData ): :rocket: investigacion, descripcion y ejemplo de implementacion

--- main.js
+++ main.js
@@ -1,52 +1,28 @@
 /* 
-  Investigacion acerca de el metodo Object.fromEntries()
+  Investigacion acerca Objetos FormData
 */
 
 /* 
-  El método Object.fromEntries() transforma una 
-  lista de pares con [clave-valor] en un objeto. 
+  Los objetos FormData le permiten compilar un conjunto 
+  de pares clave/valor para enviar mediante XMLHttpRequest. 
+  Están destinados principalmente para el envío de los datos 
+  del formulario, pero se pueden utilizar de forma independiente 
+  con el fin de transmitir los datos tecleados. Los datos transmitidos 
+  estarán en el mismo formato que usa el método submit() del 
+  formulario para enviar los datos si el tipo de codificación 
+  del formulario se establece en "multipart/form-data".
 */
-
-const entries = new Map([
-  ['foo', 'bar'],
-  ['baz', 42]
-]);
-
-const obj = Object.fromEntries(entries);
-
-console.log(obj);
-// Expected output: Object { foo: "bar", baz: 42 }
 
 
 /* 
-    Descripción:
-
-    El método Object.fromEntries() toma una lista de pares con clave-valor 
-    y devuelve un nuevo objeto cuyas propiedades son dadas por éstas entradas. 
-    El argumento iterador se espera que sea un objeto que implemente un método 
-    @@iterator, que devuelve un objeto iterador, que produce un objeto tipo 
-    array de dos elementos, donde el primer elemento es un valor que se usará 
-    como la clave de la propiedad, y el segundo elemento es el valor a 
-    asociar con esa clave de propiedad.
+  Creación de un objeto FormData desde cero
+  Usted mismo puede construir un objeto FormData instanciándolo 
+  y después añadiendo campos a la instancia usando su método append() , 
+  al y como se muestra: 
 */
 
-// convirtiendo un arreglo en un objeto 
+var formData = new FormData();
+formData.append("username", "Groucho");
+formData.append("accountnum", 123456); // number 123456 is immediately converted to string "123456"
 
-const arr = [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ];
-const obj1 = Object.fromEntries(arr);
-console.log(obj1); // { 0: "a", 1: "b", 2: "c" }
-
-
-
-// Transformacion de objetos
-
-const object1 = { a: 1, b: 2, c: 3 };
-
-const object2 = Object.fromEntries(
-  Object.entries(object1)
-  .map(([ key, val ]) => [ key, val * 2 ])
-);
-
-console.log("\nobjeto sin transformar:\n",object1);
-console.log("\nobjeto transformado:\n",object2);
-// { a: 2, b: 4, c: 6 }
+console.log(formData);
