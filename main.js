1d636febf30c806e7598e804888e75adbc3590d4
paleto30 <paleto0817@gmail.com>
2023-03-19 09:18:49 -0500

    feat ( Investigacion de funciones ) : :sparkles:  funciones de tipo declarativo y ejemplos

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 /* 
-    metodos Math
+  Investigacion acerca de los tipos de funciones 
+  que se manejan en javascript    
 */
-//  Metodo .atan2()   
 
 
-function calcAngleDegrees(x, y) {
-    return Math.atan2(y, x) * 180 / Math.PI;
-  }
-  
-  console.log(calcAngleDegrees(5, 5));
-
-  console.log(calcAngleDegrees(10, 10));
-
-  console.log(calcAngleDegrees(0, 10));
-  
-  
 /* 
-El método estático Math.atan2() devuelve el ángulo en 
-el plano (en radianes) entre el eje x positivo y el rayo 
-desde (0, 0) hasta el punto (x, y), para Math.atan2(y, x ).
+  Hay varias formas de crear funciones en Javascript: por declaración 
+  (la más usada por principiantes), por expresión 
+  (la más habitual en programadores con experiencia) o 
+  mediante constructor de objeto (no recomendada):
 */
+
+
+// sintaxis para crear una funcion declarativa 
+
+function myFunction(nombre) {
+  return `Hola ${nombre}`;
+}
+
+let nombre = "Andres Galvis"
+
+console.log(myFunction(nombre));
+
+/* 
+  Funciones por declaración
+
+Probablemente, la forma más popular de estas tres, y a la que estaremos 
+acostumbrados si venimos de otros lenguajes de programación, es la primera, 
+a la creación de funciones por declaración. Esta forma permite declarar una 
+función que existirá a lo largo de todo el código:
+
+estas  funciones se pueden ejecutar antes de haberse creado ,
+es decir puedes declararla mass abajo de donde necesitas implementarla
+  ejm:
+*/
+
+
+console.log(otraFunction());
+
+// como podemos observar primero estoy invocando la funcion en el console.log(),
+// y luego la estoy declarando (dando una estructura a dicha funcion)  
+function otraFunction() {
+  return `hola mundo`
+}
