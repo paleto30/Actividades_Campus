1573f605f9c1a4019ff9003fdf80af9fec5a4d43
paleto30 <paleto0817@gmail.com>
2023-03-19 10:14:10 -0500

    feat ( funciones como objeto ): :rocket: Este tipo de funciones realmente no se usan , pero , es bueno saber que  tambien existen

--- main.js
+++ main.js
@@ -4,39 +4,15 @@
 */
 
 /* 
-  Funciones por expresión
-
-Sin embargo, en Javascript es muy habitual encontrarse 
-códigos donde los programadores «guardan funciones» dentro de 
-variables, para posteriormente «ejecutar dichas variables». 
-se trata de un enfoque diferente, creación de funciones por 
-expresión, que fundamentalmente, hacen lo mismo con algunas diferencias:
+  Funciones como objetos 
+  
+  Como curiosidad, debes saber que se pueden declarar 
+  funciones como si fueran objetos. Sin embargo, es un 
+  enfoque que no se suele utilizar en producción. 
+  Simplemente es interesante saberlo para darse cuenta 
+  que en Javascript todo pueden ser objetos:
 */
 
-const saludo = function saludar() {
-  return "Hola";
-};
+const saludar = new Function("return 'Hola';");
 
-console.log(saludo());
-
-
-/*  
-  Con este nuevo enfoque, estamos creando una función en el interior de una variable, 
-  lo que nos permitirá posteriormente ejecutar la variable (como si fuera una función). 
-  Observa que el nombre de la función (en este ejemplo: saludar) pasa a ser inútil, 
-  ya que si intentamos ejecutar saludar() nos dirá que no existe y si intentamos 
-  ejecutar saludo() funciona correctamente.
-
-  ¿Qué ha pasado? Ahora el nombre de la función pasa a ser el nombre de la variable, 
-  mientras que el nombre de la función desaparece y se omite, dando paso a lo que se 
-  llaman las funciones anónimas (o funciones lambda).
-  ejemplo:
-*/
-
-const funcionAnonima = function (){
-    console.log("soy una funcion anonima");
-};
-
-console.log(funcionAnonima);  // como vemos , si imprimimos la variable , nos retorna su contenido
-funcionAnonima(); // y si imprimimos la variable como funcion , nos retorna la operacion de la  funcion que contiene;
- 
+console.log(saludar()); // 'Hola'
