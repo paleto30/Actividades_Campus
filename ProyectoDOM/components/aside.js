d7a7e75f7e5a1377be2d45e0094c775ccd92c24a
paleto30 <paleto0817@gmail.com>
2023-03-28 09:33:37 -0500

    feat (finishedWorker): :rocket: se finalizo el proceso del worker

--- ProyectoDOM/components/aside.js
+++ ProyectoDOM/components/aside.js
@@ -95,6 +95,7 @@ export default {
         ws.addEventListener("message",(e)=>{
             let doc = new DOMParser().parseFromString(e.data,"text/html").body;
             document.querySelector("#nav").append(...doc.children);
+            ws.terminate();
         })
     }
 
