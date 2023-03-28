ae02e2b8603b20513711ad4c8e7f12f424e39cf4
paleto30 <paleto0817@gmail.com>
2023-03-28 13:53:30 -0500

    feat ( comentario Modulos ): :art: se comentario los modulos que no he usado

--- ProyectoDOM/main.js
+++ ProyectoDOM/main.js
@@ -3,10 +3,10 @@ import banner from "./components/banner.js";
 import cards from "./components/cards.js";
 import aside from "./components/aside.js";
 import content from "./components/content.js";
-import content2 from "./components/content2.js";
+/* import content2 from "./components/content2.js";
 import content3 from "./components/content3.js";
 import footer from "./components/footer.js";
-
+ */
 
 header.fragmenShow() //-> renderizacion de el header con worker
 
@@ -15,8 +15,6 @@ banner.listBanner(); // -> renderizacion de  el banner
 cards.showRenderWorker(); // -> renderizacion de las cards con worker
 
 aside.renderWorkerData(); // -> renderizaciond del aside con el worker
-
-
 
 content.renderWorkerData();
 
