15dbe2f87d95707b77f51c16eed2b29f5926c67f
paleto30 <paleto0817@gmail.com>
2023-03-28 21:43:32 -0500

    feat ( modulos worker ): :rocket: se implemento la modularizacion de la carga de datos por medio de los modulos workers, para renderizar el contenido de los modulos content y content2

--- ProyectoDOM/main.js
+++ ProyectoDOM/main.js
@@ -3,37 +3,20 @@ import banner from "./components/banner.js";
 import cards from "./components/cards.js";
 import aside from "./components/aside.js";
 import content from "./components/content.js";
-/* import content2 from "./components/content2.js";
-import content3 from "./components/content3.js";
-import footer from "./components/footer.js";
- */
+import content2 from "./components/content2.js";
+/* import content3 from "./components/content3.js";
+import footer from "./components/footer.js"; */
+
 
 header.fragmenShow() //-> renderizacion de el header con worker
-
 banner.listBanner(); // -> renderizacion de  el banner
-
 cards.showRenderWorker(); // -> renderizacion de las cards con worker
-
 aside.renderWorkerData(); // -> renderizaciond del aside con el worker
-
-content.renderWorkerData();
-
-// contenido del body desde seccion de noticias
-//content.showNotice();
-// Primer bloque 
-//content.bloqueUno();
-//segundo bloque
-//content.bloqueDos();
-// tercer bloque
-//content.bloqueTres()
-
-
-
-/* // contenido del articulo 1
-content2.showInfo();
+content.renderWorkerData(); // -> renderizacion del content con el worker
+content2.renderWorkerData(); // -> renderizacion del content2 con el worket
 
 // contenido del articulo 2
-content3.showInfos();
+/* content3.showInfos();
 
 //footer
-footer.showData(); */
+footer.showData();  */
