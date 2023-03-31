146e7f69e32a7621b6d44ce14d75c9be900fc7f3
paleto30 <paleto0817@gmail.com>
2023-03-31 09:33:17 -0500

    feat ( worker config ): :rocket: se esta terminando de configurar el worker para el formulario

---
+++ component/formulario.js
@@ -0,0 +1,33 @@
+export default{
+
+    dataF:{
+        operacion:[
+            {
+                value:"sumar",
+                text:"+"
+            },
+            {
+                value:"restar",
+                text:"-"
+            }
+        ],
+        descripcion:{
+            name:"descripcion",
+            placeholder:"Agregar Descripcion de movimineto",
+        },
+
+        valor:{
+            name: "valor",
+            placeholder:"$0",
+            value: 0
+        }
+    },
+
+    renderDataWorker(){
+        const ws = new Worker("./storage/workerForm.js", {type:"module"});
+        ws.postMessage(this.dataF);
+        ws.addEventListener("message",(e)=>{
+            console.log(e.data);
+        });
+    }
+}
---
+++ storage/workerForm.js
@@ -0,0 +1,37 @@
+let workerForm = {
+
+    showFormulario(dataForm){
+        return `
+        <!-- operacion -->
+        <div
+            class="col-2 col-md-4 pe-2 ps-2 py-1 py-md-2 d-flex justify-content-center justify-content-sm-center justify-content-md-end">
+            <select class="form-select w-25 " name="operacion" aria-label="Default select example">
+                <option value="" selected> + </option>
+                <option value="res"> - </option>
+            </select>
+        </div>
+        <!--  input descripcion -->
+        <div class="col-10 col-md-3 d-flex justify-content-center pe-2 py-1 py-md-2">
+            <input type="text" class="form-control  text-center" placeholder="Agregar Descripcion de movimineto"
+                name="descripcion" id="descripcion">
+        </div>
+
+        <!-- input valo -->
+        <div class="col-12 col-md-2  d-flex justify-content-center pe-2 py-1 py-md-2 ps-2">
+            <input type="text" class="form-control  text-center" placeholder="$0" name="valor" id="valor" value="0">
+        </div>
+
+        <!-- submit btn -->
+        <div
+            class="col-12 col-md-3 d-flex justify-content-center justify-content-md-start px-2 pb-1 pb-md-0 align-items-center">
+            <button type="submit" class="rounded-circle  border-0 boton">
+                <i class="fa-regular fa-circle-check" style="color: #3fd942; font-size: 30px;"></i>
+            </button>
+        </div>
+        `
+    }
+}
+
+self.addEventListener("message",(e)=>{
+    console.log(e.data.operacion);
+})
