828bc92501309f4d07176a678f07d441064ae285
paleto30 <paleto0817@gmail.com>
2023-04-18 17:30:37 -0500

    feat ( Callbacks ): :rocket: se investiga y se hacen ejemplos para entender el concepto de callback en javascript

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 /* 
-  Investigacion acerca Objetos FormData
+  ¿QUE ES UN CALLBACK?
 */
 
 /* 
-  Los objetos FormData le permiten compilar un conjunto 
-  de pares clave/valor para enviar mediante XMLHttpRequest. 
-  Están destinados principalmente para el envío de los datos 
-  del formulario, pero se pueden utilizar de forma independiente 
-  con el fin de transmitir los datos tecleados. Los datos transmitidos 
-  estarán en el mismo formato que usa el método submit() del 
-  formulario para enviar los datos si el tipo de codificación 
-  del formulario se establece en "multipart/form-data".
+  Como hemos dicho, las funciones callback no son más que un 
+  tipo de funciones que se pasan por parámetro a otras funciones. 
+  Además, los parámetros de dichas funciones toman un valor especial 
+  en el contexto del interior de la función.
+
+  En resumen, las funciones callback son una forma de manejar 
+  eventos y acciones en un programa de manera modular y escalable. 
+  Permiten que una función llame a otra función para manejar una 
+  tarea específica, lo que hace que el código sea más fácil de 
+  mantener y entender
 */
 
 
 /* 
-  Creación de un objeto FormData desde cero
-  Usted mismo puede construir un objeto FormData instanciándolo 
-  y después añadiendo campos a la instancia usando su método append() , 
-  al y como se muestra: 
+  por ejemplo si yo tengo un array de datos y quiero recorrer el array 
+  puedo hacer uso de un callback 
+  de la siguiente manera:
 */
 
-var formData = new FormData();
-formData.append("username", "Groucho");
-formData.append("accountnum", 123456); // number 123456 is immediately converted to string "123456"
+const personas = ["Andres","Camilo","Sergio","Angie"];
 
-console.log(formData);
+personas.forEach((element, index) =>{     // como podemos observar se le esta pasando una funcion ()=>{}
+    console.log("Index: ",index," ","elemento: ",element,);          // como parametro a la funcion forEach();
+});
+
+
+// otro ejemplo de callback es el siguiennte
+// tenemos la funcion setTimeout(acc,time); recibe una funcion y un tiempo en milisegundos
+
+setTimeout(()=>{console.log("se ejecuta el callbackc");},2000);
